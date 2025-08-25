cc21f8fb8896613d8029892ce44beb415dc35a49
Isadora Santos Feijó <isadora.feijo2006@gmail.com>
2025-08-25 19:52:38 -0300

    Update sálario_aumento.c

--- Linguagem de Programação - l/Exercícios.Aula1/sálario_aumento.c
+++ Linguagem de Programação - l/Exercícios.Aula1/sálario_aumento.c
@@ -1,5 +1,9 @@
-
 #include <stdio.h>
+
+/*
+Considerando que o aumento dos funcionários é de 75% do INPC e mais um percentual de produtividade discutido com a empresa. 
+Construir um programa que lê o número do funcionário, seu salário atual, o valor do INPC e o índice de produtividade e escreve o número do funcionário, seu aumento e o valor de seu novo salário.
+*/
 
 int main()
 {   
