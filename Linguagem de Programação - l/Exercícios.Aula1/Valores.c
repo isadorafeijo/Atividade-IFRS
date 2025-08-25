2c628b39cef44e5d59376523ff565b462af7c04d
Isadora Santos Feijó <isadora.feijo2006@gmail.com>
2025-08-25 19:39:55 -0300

    Update Valores.c

--- Linguagem de Programação - l/Exercícios.Aula1/Valores.c
+++ Linguagem de Programação - l/Exercícios.Aula1/Valores.c
@@ -1,5 +1,10 @@
-
 #include <stdio.h>
+
+/*
+A Joalheria Silva está fazendo aniversário e resolveu premiar o primeiro cliente do dia devolvendo a ele o valor da compra em dobro.
+Faça um programa que leia o preço unitário do produto adquirido pelo cliente, a quantidade e calcule o total gasto pelo cliente. 
+Mostre o total e calcule o prêmio. Mostre o valor do prêmio (total da compra multiplicado por 2)
+*/
 
 int main()
 {   
