0ef569cf183c0f4035f27fe66014676bad64a351
Isadora Santos Feijó <isadora.feijo2006@gmail.com>
2025-08-25 19:18:00 -0300

    Update media, nota e falta.c

--- Exercícios.aula2/media, nota e falta.c
+++ Exercícios.aula2/media, nota e falta.c
@@ -1,5 +1,11 @@
-
 #include <stdio.h>
+
+/*
+Faça um programa que leia a média do aluno e seu numero de faltas.
+    se as faltas forem maior ou igual a 5, escreva 'reprovado por faltas'.
+    Se a média for menor que 7, foi reprovado por média. 
+    Senão, escreva 'Parabéns! você foi aprovado'.
+*/
 
 int main()
 {
